56c4221d84738e71c3088fa97602a2bdefc02b37
Johnny <41584622+jonipwi@users.noreply.github.com>
2026-05-13 22:33:46 +0700

    Remove duplicate lines in setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 # RUN AS ADMINISTRATOR
 
 cp ./configs/config.example.yaml ./configs/config.yaml
 
 winguard install-service --config ./configs/config.yaml
 
-=======
-# RUN AS ADMINISTRATOR
-
-cp ./configs/config.example.yaml ./configs/config.yaml
-
-winguard install-service --config ./configs/config.yaml
-
->>>>>>> origin/main
 sc.exe start WinGuardGo
